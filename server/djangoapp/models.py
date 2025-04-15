daa346db65a39495d349170e124d9201420376f9
Shreyash Sanjay <80293646+ShreyashSanjay@users.noreply.github.com>
2025-04-15 16:10:28 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -27,6 +27,5 @@ class CarModel(models.Model):
             MinValueValidator(2015)])
     # Other fields as needed
 
-    
     def __str__(self):
         return self.name  # Return the name as the string representation
