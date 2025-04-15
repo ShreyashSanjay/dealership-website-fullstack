fdb6c3202203515fd72e93b5d19b791da9a33629
Shreyash Sanjay <shreyash6@hotmail.co.uk>
2025-04-15 08:09:35 -0400

    Created a proxy server for cloud functions

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     # path for dealer reviews view
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import CarMake, CarModel
 from .populate import initiate
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 
 # Get an instance of a logger
@@ -42,7 +43,6 @@ def logout_request(request):
     logout(request)
     data = {"userName":""}
     return JsonResponse(data)
-
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
@@ -102,6 +102,15 @@ def get_cars(request):
 # def get_dealer_details(request, dealer_id):
 # ...
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+# submit a review
+def add_review(request):
+    # Check auth of user
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
