2edf4bc77401a1af97028fc11311a6d8bbf1d177
Shreyash Sanjay <80293646+ShreyashSanjay@users.noreply.github.com>
2025-04-15 15:59:34 +0100

    Update models.py
    Better presentation

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,26 +1,26 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    
     def __str__(self):
         return self.name
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        # Add more choices as required
-    ]
+        ('WAGON', 'Wagon'),]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
         validators=[
@@ -29,5 +29,6 @@ class CarModel(models.Model):
         ])
     # Other fields as needed
 
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
